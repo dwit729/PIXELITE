6679b235eafc07a3211815ff2fc1db56d6b2f6ac
Sofia <sofiasimoune.ilustre@gmail.com>
2024-06-14 14:50:19 +0800

    About page complete

--- client/src/components/MainNavbar.jsx
+++ client/src/components/MainNavbar.jsx
@@ -1,30 +1,39 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 function NavBar() {
   return (
     <Navbar expand="lg" className="bg-body-secondary">
       <Container>
         <Navbar.Brand href="#home">PIXELITE</Navbar.Brand>
-        <div className='d-flex flex-row-reverse'>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav" className='me-1'>
+        <div className="d-flex flex-row-reverse">
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav" className="me-1">
             <Nav>
-            <Nav.Link href="#" className='mx-2'>Home</Nav.Link>
-            <Nav.Link href="#link" className='mx-2'>About Us</Nav.Link>
-            <Nav.Link href="#link" className='mx-2'>Login</Nav.Link>
-            <NavDropdown title="Services" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Photoshoot</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Party Planner
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Event Organizer <br></br>and Photographer</NavDropdown.Item>
-              
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
+              <Nav.Link href="#" className="mx-2">
+                Home
+              </Nav.Link>
+              <Nav.Link href="#link" className="mx-2">
+                About Us
+              </Nav.Link>
+              <Nav.Link href="#link" className="mx-2">
+                Login
+              </Nav.Link>
+              <NavDropdown title="Services" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">
+                  Photoshoot
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Party Planner
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Event Organizer <br></br>and Photographer
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
         </div>
       </Container>
     </Navbar>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import Pixelite from './Pixelite.jsx'
 import LoginForm from './components/LoginForm.jsx'
 
+import About from './pages/About.jsx'
+
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  <Pixelite/>
+    <About/>
   </React.StrictMode>
 )
