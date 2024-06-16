ac50c9d4ba4d7e1b99923df46f2f634207ced906
Sofia <sofiasimoune.ilustre@gmail.com>
2024-06-17 00:24:34 +0800

    Added a Register Page, complete with validation? padagdagan na lang if kulang pa hehe

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from "react-dom/client";
 import Pixelite from "./Pixelite.jsx";
 import LoginForm from "./components/LoginForm.jsx";
 import ClientProfile from "./pages/ClientProfile.jsx";
+import Register from "./pages/Register.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Pixelite />
+    <Register />
   </React.StrictMode>
 );
